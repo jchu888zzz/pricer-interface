92a95fde4ee0e2386fbaeff6ad170545f9528928
Your Name <237002223+jchu888zzz@users.noreply.github.com>
2025-12-26 11:36:32 +0100

    adjust main_window to previous changes

--- src/app/main_window.py
+++ src/app/main_window.py
@@ -1,21 +1,14 @@
 from PySide6.QtWidgets import QMainWindow, QWidget, QButtonGroup,QMessageBox
-from PySide6.QtCore import QThread,QThreadPool
+from PySide6.QtGui import QCloseEvent
 import os
 import QuantLib as ql
 import pandas as pd
 
-from .workers.CustomWorkers import SnapshotWorker,RateWorker,CMTWorker,EquityWorker
+from .workers.CustomWorkers import PriceManager,MktDataManager
 from .ui_pages.MainWindow import Ui_MainWindow
-from .result_dialog import  ResultDialog,ResultDialogEquity
+from .result_dialog import  ResultDialog
 
-
-def _setup_result_worker_to_thread(thread,worker,result_dialog):
-    result_dialog.show_loading_page()
-    worker.finished.connect(result_dialog.show_table_page)
-    worker.finished.connect(thread.quit)
-    worker.finished.connect(worker.deleteLater)
-    thread.finished.connect(thread.deleteLater)
-    thread.start()
+import Pricing.Rates.GetResults as RateGetResults
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -28,6 +21,9 @@ class MainWindow(QMainWindow):
         #self.menuBar().hide()
         self.statusBar().hide()
 
+        self.pricing_manager=PriceManager(dialog_class=ResultDialog)
+        self.pricing_manager.start()
+        self.data_manager=MktDataManager()
 
     def _apply_style_from_qss(self, path: str = None):
         """
@@ -47,61 +43,28 @@ class MainWindow(QMainWindow):
 
     def load_data(self):
         self.ui.pageHome.action_button.setEnabled(False)
-        date=ql.Date.todaysDate()
-        self.thread=QThread()
-        self.worker=SnapshotWorker(date)
-        self.worker.moveToThread(self.thread)
-
-        self.thread.started.connect(self.worker.run)
         self.ui.pageHome._animate_loading()
-        self.worker.finished.connect(self.retrieve_mkt_data)
-        self.worker.finished_msg.connect(self.ui.pageHome.setMessage)
-        self.worker.finished.connect(self.thread.quit)
-        self.worker.finished.connect(self.worker.deleteLater)
-        self.thread.finished.connect(self.thread.deleteLater)
-        self.thread.start()
+        self.data_manager.completed.connect(self.retrieve_mkt_data)
+        self.data_manager.failed.connect(lambda err: self.ui.pageHome.setMessage(err))
+        #date=ql.Date.todaysDate()
+        date=ql.Date(11,11,2025)
+        self.data_manager.retrieve(date)
 
-    def retrieve_mkt_data(self,dic_dataframe:dict[str:pd.DataFrame]):
+    def retrieve_mkt_data(self,mkt_data:dict[str:pd.DataFrame]):
         self.ui.pageHome.action_button.setEnabled(True)
-        self.dic_dataframe=dic_dataframe
-    
-    def check_data_loaded(self):
-        if not hasattr(self,'dic_dataframe'):
+        self.ui.pageHome._timer.stop()
+        self.ui.pageHome.loading_label.setText("Market Data imported")
+        self.mkt_data=mkt_data
+
+    def get_result(self,input:dict):
+        if not hasattr(self,'mkt_data'):
             QMessageBox.warning(self, "Validation error", "Please load data before pricing")
-
-    def result_for_rate_product(self,dic_contract:dict):
-        self.check_data_loaded()
-        self.thread=QThread()
-        self.worker=RateWorker(self.dic_dataframe,dic_contract)
-        self.worker.moveToThread(self.thread)
+        PAGE_MAPPING={"Rate":RateGetResults.compute_result_rate,
+                      "CMT":RateGetResults.compute_result_cmt,
+                      "SpreadCMT":RateGetResults.compute_result_cmt}
         
-        if "UF" in dic_contract['param']:
-            self.thread.started.connect(self.worker.solve_coupon)
-        else:
-            self.thread.started.connect(self.worker.compute_price)
-        self.result_dialog = ResultDialog()
-        _setup_result_worker_to_thread(self.thread,self.worker,self.result_dialog)
-    
-    def result_for_cmt_product(self,dic_contract:dict):
-        self.check_data_loaded()
-        self.thread=QThread()
-        self.worker=CMTWorker(self.dic_dataframe,dic_contract)
-        self.worker.moveToThread(self.thread)
-        if "UF" in dic_contract['param']:
-            self.thread.started.connect(self.worker.solve_coupon)
-        else:
-            self.thread.started.connect(self.worker.compute_price)
-        self.result_dialog = ResultDialog()
-        _setup_result_worker_to_thread(self.thread,self.worker,self.result_dialog)
-
-    def result_for_equity_product(self,dic_contract:dict):
-        self.thread=QThread()
-        self.worker=EquityWorker(dic_contract)
-        self.worker.moveToThread(self.thread)
-        self.thread.started.connect(self.worker.compute_result)
-        self.result_dialog = ResultDialogEquity()
-        _setup_result_worker_to_thread(self.thread,self.worker,self.result_dialog)
-
+        self.pricing_manager.add_task("task_rate",PAGE_MAPPING.get(input["_source_page"]),args=(self.mkt_data,input))
+        
 
     def _setup_logic(self):
         # Wire nav buttons to stacked widget pages
@@ -112,11 +75,10 @@ class MainWindow(QMainWindow):
         self.ui.btnSpreadCMT.clicked.connect(lambda: self._set_page(self.ui.pageSpreadCMT))
 
         self.ui.pageHome.action_button.clicked.connect(self.load_data)
-        self.ui.pageRate.submitted.connect(self.result_for_rate_product)
-        self.ui.pageCMT.submitted.connect( self.result_for_cmt_product )
-        self.ui.pageSpreadCMT.submitted.connect( self.result_for_cmt_product )
-        self.ui.pageEquity.submitted.connect( self.result_for_equity_product )
-        
+        self.ui.pageRate.submitted.connect(self.get_result)
+        self.ui.pageCMT.submitted.connect(self.get_result)
+        self.ui.pageSpreadCMT.submitted.connect(self.get_result)
+
         # Keep nav buttons exclusive
         self._nav_group = QButtonGroup(self)
         self._nav_group.setExclusive(True)
@@ -135,6 +97,24 @@ class MainWindow(QMainWindow):
 
     def _set_page(self, page:QWidget):
         self.ui.stack.setCurrentWidget(page)
+
+    def closeEvent(self, event: QCloseEvent):
+        """Clean up threads before closing."""
+        try:
+            if hasattr(self, 'pricing_manager'):
+                self.pricing_manager.stop()
+        except:
+            pass
+        
+        try:
+            if hasattr(self, 'data_manager') and hasattr(self.data_manager, 'thread'):
+                if self.data_manager.thread and self.data_manager.thread.isRunning():
+                    self.data_manager.thread.quit()
+                    self.data_manager.thread.wait()
+        except:
+            pass
+        
+        event.accept()
         # update checked states handled by QButtonGroup
 
 
