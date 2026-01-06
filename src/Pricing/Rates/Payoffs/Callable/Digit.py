359daaccea77e7cd22167da17c5fb9efe3eb1c6f
jchu888zzz <237002223+jchu888zzz@users.noreply.github.com>
2026-01-06 17:18:00 +0100

    Update Digit.py

--- src/Pricing/Rates/Payoffs/Callable/Digit.py
+++ src/Pricing/Rates/Payoffs/Callable/Digit.py
@@ -11,7 +11,7 @@ def precomputation(calc_date:ql.Date,model,data:dict[str:str],risky_curve,risky:
     return CallableFeature.prep_callable_contract(calc_date,contract,model,risky_curve,risky)
 
 
-REGRESSOR_CLASS=KNeighborsRegressor(n_neighbors=20)
+REGRESSOR_CLASS=KNeighborsRegressor(n_neighbors=30)
 def compute_price(dic_prep:dict,risky_curve):
     return CallableFeature.compute_price(dic_prep,risky_curve,
                                             basis_option='polynomial',
@@ -21,28 +21,6 @@ def solve_coupon(dic_prep:dict,risky_curve):
     return CallableFeature.solve_coupon(dic_prep,risky_curve,
                                         basis_option='polynomial',
                                         regressor_class=REGRESSOR_CLASS)
-
-
-class Process :
-    def compute_price(prep_model:dict,param_contract:dict):
-        dic_prep=precomputation(prep_model['calc_date'],prep_model['model'],
-                                param_contract,prep_model['risky_curve'],risky=True)
-
-        return compute_price(dic_prep,prep_model['risky_curve'])
-        
-    def solve_coupon(prep_model:dict,param_contract:dict):
-        dic_prep=precomputation(prep_model['calc_date'],prep_model['model'],
-                                param_contract,prep_model['risky_curve'],risky=True)
-        
-        def update_dic_prep(coupon,spread) ->dict:
-            dic_prep_new=dic_prep.copy()
-            dic_prep_new['contract'].coupon=coupon
-            dic_prep_new['contract'].funding_spread=spread
-            return dic_prep_new
-
-        coupon,spread=solve_coupon(dic_prep,prep_model['risky_curve'])
-        dic_prep_new=update_dic_prep(coupon,spread)
-        return compute_price(dic_prep_new,prep_model['risky_curve'])
 
 class Digit(Base.Payoff):
     def __init__(self,parameters:dict):
